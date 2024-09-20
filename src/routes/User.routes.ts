83073c7431b89e5522dd9d8a9601aad0de9c812c
Uneebbhat <uneebbhatti3@gmail.com>
2024-09-20 11:20:33 +0500

    route updated

--- src/routes/User.routes.ts
+++ src/routes/User.routes.ts
@@ -1,10 +1,9 @@
 import { Router } from "express";
-import { login, otpSignup, signup } from "../controllers/UserController";
+import { login, signup } from "../controllers/UserController";
 
 const router = Router();
 
 router.post("/v1/signup", signup);
 router.post("/v1/login", login);
-router.post("/sendotp", otpSignup);
 
 export default router;
